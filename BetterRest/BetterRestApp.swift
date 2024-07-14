390ad7063157e9b5236db1ddabde5543c140114e
ayrton <ayrton.parkinson1@gmail.com>
2024-07-14 07:18:23 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Ayrton Parkinson on 2024/07/14.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
