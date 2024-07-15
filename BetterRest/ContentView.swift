deefbe7594201a5a616841e7a4cd10280460b397
ayrton <ayrton.parkinson1@gmail.com>
2024-07-15 07:14:13 -0400

    Remove old calculate bedtime method

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -53,30 +53,6 @@ struct ContentView: View {
         }
     }
     
-    func calculateBedtime() {
-        do {
-            let config = MLModelConfiguration()
-            let model = try SleepCalculator(configuration: config)
-            
-            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
-            let hour = (components.hour ?? 0) * 60 * 60
-            let minute = (components.minute ?? 0) * 60
-            
-            let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
-            
-            let sleepTime = wakeUp - prediction.actualSleep
-            print(type(of: sleepTime))
-            
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
-        } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, there was a problem calculating your bedtime"
-        }
-        
-        showingAlert = true
-    }
-    
     private var bedtime: String {
         do {
             let config = MLModelConfiguration()
